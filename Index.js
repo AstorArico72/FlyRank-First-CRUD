f420e0f12d2a8e4ccb8accf3bfb3289672877711
Astor Aricó <astorarico72@gmail.com>
2026-09-14 10:56:23 -0300

    Stage 1: Root and health endpoints Cambios a ser confirmados: 	modificados:     Index.js

--- Index.js
+++ Index.js
@@ -3,7 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+
+  let JsonResponse = {
+    "name": "Task API",
+    "version": "1.0",
+    "endpoints": ["/tasks"]
+  }
+  res.send(JsonResponse);
+});
+
+app.get("/health", (req, res) => {
+  let JsonResponse = {
+    "status": "ok"
+  }
+  res.send (JsonResponse);
 });
 
 app.listen(port, () => {
