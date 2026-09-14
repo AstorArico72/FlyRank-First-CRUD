34e6a53425e183479157c25036715977f9d4a77d
Astor Aricó <astorarico72@gmail.com>
2026-09-14 11:14:52 -0300

    Stage 2: Read endpoints with 404

--- Index.js
+++ Index.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var TasksList = [
+  {
+    "id": 1,
+    "title": "Create a lisk of tasks",
+    "done": true
+  },
+  {
+    "id": 2,
+    "title": "Get a job",
+    "done": false
+  },
+  {
+    "id": 3,
+    "title": "Have a degree",
+    "done": true
+  }
+];
+
 app.get('/', (req, res) => {
 
   let JsonResponse = {
@@ -17,6 +35,18 @@ app.get("/health", (req, res) => {
     "status": "ok"
   }
   res.send (JsonResponse);
+});
+
+app.get("/tasks/:id", (req, res) => {
+  let task = TasksList.find (item => item.id == req.params.id);
+  let JsonError = {
+    "error": "Task " + req.params.id + " not found."
+  };
+  if (task) {
+    res.send (task);
+  } else {
+    res.status (404).send (JsonError);
+  }
 });
 
 app.listen(port, () => {
